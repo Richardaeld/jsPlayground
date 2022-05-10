5aa762102dcea8b8d81ca09592b5103cde5ee890
Richardaeld <Richardaeld@gmail.com>
2022-05-10 18:38:13 -0400

    func expression/declaration and higher order functions

---
+++ funcDeclarationExpression.js
@@ -0,0 +1,9 @@
+// Function Declaration
+function funcD () {
+    console.log('Function Declaration')
+}
+
+// Function Expression
+let funcE = function () {
+    console.log('function Expression')
+}
---
+++ higherOrderFunction.js
@@ -0,0 +1,28 @@
+const  radius = [3, 1, 2, 4]
+
+// Area PI r^2
+const area = function (radius) {
+    return Math.PI * radius * radius;
+};
+
+// cicumference 2 PI r
+const circumference = function (radius) {
+    return 2 * Math.PI * radius;
+};
+
+// diameter 2r
+const diameter = function (radius) {
+    return radius * 2
+}
+
+const calculate = function (radius, logic) {
+    const output = []
+    for (let i = 0; i < radius.length; i++) {
+        output.push(logic(radius[i]))
+    }
+    return output
+};
+
+console.log(calculate(radius, area))
+console.log(calculate(radius, circumference))
+console.log(calculate(radius, diameter))
