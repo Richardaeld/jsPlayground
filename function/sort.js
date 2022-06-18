3ed5af06f15251c33c3dafe7aa387dd403a8132b
Richardaeld <Richardaeld@gmail.com>
2022-06-18 18:19:30 -0400

    Added num/alpha sort

--- function/sort.js
+++ function/sort.js
@@ -17,3 +17,22 @@ function sortDescend (array) {
 console.log(sortDescend(numArr))
 console.log(sortDescend(emptyArray))
 
+const alphaNumAray = [1, 100000, 21 ,30 ,4, "Goat", "Vipis", "Omeron", "Bitis"]
+// console.log(alphaNumAray)
+
+function alphaNumAscend (array) {
+    if (!array.length) return;
+    const num = [];
+    const alpha = [];
+
+    array.forEach((element) => {
+        isNaN(element) ? alpha.push(element) : num.push(element)
+    });
+
+    num.sort((a,b) => a -b);
+    alpha.sort();
+    return [alpha + num]
+}
+
+console.log(alphaNumAscend(alphaNumAray))
+console.log(alphaNumAscend(emptyArray))
