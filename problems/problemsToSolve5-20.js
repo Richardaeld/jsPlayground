ad55b47e3beb152fa8320397487a183a86b405fa
Richardaeld <Richardaeld@gmail.com>
2022-06-21 13:00:48 -0400

    Created a list of problems to solve today

---
+++ problems/problemsToSolve5-20.js
@@ -0,0 +1,10 @@
+// Flatten with recursin
+// pascal triangle
+// Bubble sort
+// Arguments vs params
+
+// review functions
+    // .test() -> regex
+    // Array.isArray(array) 
+    // indexOf()
+    // 
