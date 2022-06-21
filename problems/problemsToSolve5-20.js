a85589d1ec7b0b1d119aeab429237631358b9c4e
Richardaeld <Richardaeld@gmail.com>
2022-06-21 17:23:24 -0400

    Completed bubble sort

--- problems/problemsToSolve5-20.js
+++ problems/problemsToSolve5-20.js
@@ -8,3 +8,23 @@
     // Array.isArray(array) 
     // indexOf()
     // 
+
+
+
+
+const bubbleSortArray = [1,4,2,5,-2,3];
+function bubbleSort (array) {
+    // newArray = []
+    for (let i=1; i < array.length; i++) {
+        // console.log(i)
+        if (array[i-1] > array[i]) {
+            const removed = array[i];
+            array.splice(i, 1);
+            array.splice(i-1, 0, removed);
+            bubbleSort(array);
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSort(bubbleSortArray));
